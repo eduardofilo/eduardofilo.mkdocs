4d1a9d60c5f7f899c9c40e09ad23b3a2c821681b
Eduardo Moreno <hmitsurugi@gmail.com>
2021-10-30 19:18:07 +0200

    Pull de eduardofilo.github.io al hacer deploy.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 cd ~/git/eduardofilo.mkdocs
 source /home/edumoreno/.virtualenvs/eduardofilo.mkdocs/bin/activate
 cd ~/git/eduardofilo.github.io
+git pull origin
 git reset HEAD .
 git checkout -- .
 mkdocs gh-deploy --config-file ../eduardofilo.mkdocs/mkdocs.yml --remote-branch master
